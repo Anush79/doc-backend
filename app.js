01e3babbea16adcc5b3bab582137950afa73f74f
Anushka J <66175237+Anush79@users.noreply.github.com>
2024-01-19 21:15:56 +0530

    added one doctor

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
+app.get("/doctors", (req, res) => {
+  res.status(200).json({
+    "name":"Shilpi Gupta",
+    "city": "Darbhanga"
+  })
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
