1675ce7229dfbbfd35191efc129b06fb498d7f6f
Anushka J <66175237+Anush79@users.noreply.github.com>
2024-01-19 23:31:47 +0530

    FIX: file import

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const {doctors} = require("./doctors")
+const {doctors} = require("./data.js")
 app.get("/", (req, res) =>  res.send(
   "<h3>Hello ,My name is Anushka Jaiswal and you are watching Doctors API.</h3>",
 ));
